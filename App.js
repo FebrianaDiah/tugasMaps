28a057202a3c6692b180ce578226e610b438e5f4
Febriana Diah Widiani <36920018+FebrianaDiah@users.noreply.github.com>
2018-05-26 19:46:49 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
+import { Marker } from 'react-native-maps';
+export default class App extends Component {
+
+  state = {
+    region: {
+      latitude: -8.149407,
+      longitude: 115.216667,
+      latitudeDelta: 0.8922,
+      longitudeDelta: 0.8421,
+    },
+    markers : [
+      {
+        key:1,
+        latlng: {
+          latitude:-8.112439,
+          longitude: 115.082869
+        },
+        title: 'Dewa Dewi Collection Singaraja',
+        subtitle: ''
+      },
+      {
+        key:2,
+        latlng: {
+          latitude:-8.636890,
+          longitude: 115.147295
+        },
+        title: 'Dewa Dewi Collection Canggu',
+        subtitle: ''
+      },
+      {
+        key:3,
+        latlng: {
+          latitude:-8.606339,
+          longitude: 115.112104
+        },
+        title: 'Dewa Dewi Collection Tabanan'
+      }
+      ,
+   ]
+  };
+
+  render() {
+    return (
+      <View style={styles.contMain}>
+        <View style={styles.contHeader}>
+            <Text style={styles.textHeader}>
+              Letak Dewa Dewi Collection
+            </Text>
+        </View>
+        <View style={styles.contMaps}>
+              <MapView
+                style={styles.map}
+                region={this.state.region}
+              >
+              {this.state.markers.map(mark => (
+              <Marker
+                  key = {mark.key}
+                  coordinate={mark.latlng}
+                  title={mark.title}
+                  description={mark.subtitle}
+                />
+              ))}
+              </MapView>
+        </View>
+        <View style={styles.contFooter}>
+           <Text style={styles.textFooter}> http://febryanadyah20@gmail.com </Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  contMain: {
+      flex : 1
+  },
+  contHeader: {
+    backgroundColor: '#9575cd',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    position: 'relative'
+  },
+  contMaps : {
+    flex : 10
+  },
+  textHeader: {
+    fontSize: 20,
+    color: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  contFooter: {
+    backgroundColor: '#9575cd',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    position: 'relative'
+  },
+  textFooter: {
+    fontSize: 16,
+    color: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+
+});
